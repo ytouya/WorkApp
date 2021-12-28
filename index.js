89be3b16e1c8aa0050a37bfb465c7b0eb677856f
ytouya <yamazawa.touya@gmail.com>
2021-12-28 20:17:51 +0900

    postgresql接続

--- index.js
+++ index.js
@@ -13,14 +13,34 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const times = process.env.TIMES || 5
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  
   .get('/times', (req, res) => res.send(countHitsuji())) // ←追記
+
   .get('/env', (req, res) => res.send(process.env.NAME))
+
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM member');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   // ↓追記：countHitsuji()
