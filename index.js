1dde3e9ace12d68bf4dc46a3b98c141404045177
ytouya <yamazawa.touya@gmail.com>
2021-12-28 13:52:08 +0900

    環境変数設定を追加

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -7,4 +18,16 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/times', (req, res) => res.send(countHitsuji())) // ←追記
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  // ↓追記：countHitsuji()
+  countHitsuji = () => {
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (i = 0; i < times; i++) {
+      // result += i + ' '
+      result += '羊が' + i + '匹 '
+    }
+    return result;
+  }
